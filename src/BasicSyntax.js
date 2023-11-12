f34fe0c1faf2ef92252116c8f1b6618cc6797a2a
Эльвина <Elvina2604@yandex.ru>
2023-11-12 20:22:37 +0300

    Практика

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,17 @@
 export function romanToInteger(str) {
+    const arabs = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    for (let i = 0; i < str.length; i++) {
+        const currentNumber = arabs[str[i]];
+        const nextNumber = arabs[str[i + 1]];
+
+        if (!nextNumber || currentNumber >= nextNumber) {
+            result += currentNumber;
+        }
+
+        if (currentNumber < nextNumber) {
+            result -= currentNumber;
+        }
+    }
     return result;
 }
